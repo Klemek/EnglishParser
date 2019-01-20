3307d39b78ded7e9f76a4485af41acb222e7a402
Klemek <Klemek@outlook.fr>
2019-01-20 19:53:20 +0100

    CI : build support + code coverage

---
+++ README.md
@@ -0,0 +1,6 @@
+# EnglishParser
+
+[![Build Status](https://img.shields.io/travis/Klemek/Villes.svg?style=popout)](https://travis-ci.org/Klemek/Villes)
+[![Coverage Status](https://img.shields.io/coveralls/github/Klemek/Villes.svg)](https://coveralls.io/github/Klemek/Villes?branch=master)
+
+WIP
--- src/main/java/fr/klemek/englishparser/Launch.java
+++ src/main/java/fr/klemek/englishparser/Launch.java
@@ -1,5 +1,6 @@
 package fr.klemek.englishparser;
 
+import fr.klemek.englishparser.model.dict.Word;
 import fr.klemek.englishparser.utils.DatabaseManager;
 import fr.klemek.englishparser.utils.DictionaryManager;
 import fr.klemek.englishparser.utils.Utils;
@@ -7,6 +8,10 @@ import fr.klemek.logger.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.EnumMap;
+import java.util.List;
 
 public class Launch {
 
@@ -22,20 +27,43 @@ public class Launch {
         testRead();
     }
 
-    private static void testRead(){
+    private static void testRead() {
         StringBuilder data = new StringBuilder();
         try (BufferedReader reader = Utils.readFile("test_res/a_very_short_story.txt")) {
             reader.readLine(); //skip first line
             String line;
-            while((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 line = line.trim();
-                if(!line.endsWith(" "))
+                if (!line.endsWith(" "))
                     line += " ";
                 data.append(line);
             }
         } catch (IOException e) {
             Logger.log(e);
         }
-        Logger.log(data.toString());
+        String[] sentences = data.toString().trim().split("\\. ");
+        analyseSentence(sentences[0]);
+    }
+
+    private static void analyseSentence(String s) {
+        Logger.log(s);
+        for (String word : s.split(" "))
+            analyseWord(word);
+    }
+
+    private static void analyseWord(String w) {
+        Logger.log(w);
+
+        EnumMap<Word.Type, Integer> p = new EnumMap<>(Word.Type.class);
+        List<Word> found = Word.getByWord(w.toLowerCase());
+        for (Word word : found)
+            p.put(word.getType(), p.getOrDefault(word.getType(), 0) + 1);
+
+        Word.Type[] types = Word.Type.values();
+        Arrays.sort(types, Comparator.comparingInt(o -> -p.getOrDefault(o, 0)));
+        for (Word.Type t : types) {
+            if (p.containsKey(t))
+                Logger.log("\t{0} {1}%", t, 100 * p.get(t) / (float) found.size());
+        }
     }
 }
