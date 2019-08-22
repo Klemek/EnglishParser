53879f5b8c2526910845fb62f5249dc57b87e5a8
Klemek <klemek@outlook.fr>
2019-08-22 11:50:24 +0200

    Code factorization

--- net-project/EnglishParser/EnglishParser.DB.Tests/DatabaseManagerTests.cs
+++ net-project/EnglishParser/EnglishParser.DB.Tests/DatabaseManagerTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 using Nini.Config;
 using NUnit.Framework;
 
@@ -5,17 +8,50 @@ namespace EnglishParser.DB.Tests
 {
     public class DatabaseManagerTests
     {
+        private static IConfig _config;
+
+        private void CleanDatabase()
+        {
+            using (MySqlConnection conn = DatabaseManager.Connect(true))
+                CleanDatabase(conn);
+        }
+        
+        private void CleanDatabase(MySqlConnection conn)
+        {
+            DatabaseManager.ImportSql(typeof(DatabaseManager).Assembly, conn, "sql/clean.sql");
+        }
+        
         [SetUp]
         public void Setup()
         {
-            IConfigSource source = new IniConfigSource("TestDatabase.ini");
-            DatabaseManager.Init(source.Configs["Database"]);
+            _config = new IniConfigSource("TestDatabase.ini").Configs["Database"];
+            DatabaseManager.Init(_config);
         }
 
         [Test]
-        public void Test1()
+        public void ListTables()
         {
-            Assert.Pass();
+            using (MySqlConnection conn = DatabaseManager.Connect(true))
+            {
+                List<string> tables = DatabaseManager.ListTables(conn);
+                Assert.Contains("db_info", tables);
+                Assert.Greater(tables.Count, 1);
+                CleanDatabase(conn);
+                tables = DatabaseManager.ListTables(conn);
+                Assert.AreEqual(0, tables.Count);
+            }
+        }
+        
+        [Test]
+        public void TableExists()
+        {
+            using (MySqlConnection conn = DatabaseManager.Connect(true))
+            {
+                Assert.True(DatabaseManager.TableExists(conn, "db_info"));
+                Assert.False(DatabaseManager.TableExists(conn, "db_info_2"));
+                CleanDatabase(conn);
+                Assert.False(DatabaseManager.TableExists(conn, "db_info"));
+            }
         }
     }
 }
